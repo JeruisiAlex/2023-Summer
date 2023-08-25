9943f6636d9e6b3e6b89684a53d7b3dcc028b254
JeruisiAlex <1525772623@qq.com>
2023-08-25 16:17:55 +0800

    20230825 1617 Jeruisi

--- src/main.js
+++ src/main.js
@@ -7,20 +7,12 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-//1
-import Vue3DraggableResizable from 'vue3-draggable-resizable'
-import 'vue3-draggable-resizable/dist/Vue3DraggableResizable.css'
-
-//2
-import NgFormElement  from 'ng-form-element'
-import 'ng-form-element/lib/ng-form-element.css'
-
 //3
 import NgFormElementPlus  from 'ng-form-elementplus'
 import 'ng-form-elementplus/lib/style.css'
 
 const app = createApp(App)
-app.use(store).use(router).use(ElementPlus).use(Vue3DraggableResizable).use(NgFormElementPlus).mount('#app')
+app.use(store).use(router).use(ElementPlus).use(NgFormElementPlus).mount('#app')
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
