9b6ea2c4dfad726422f203ee757be1e9966b4f07
JeruisiAlex <1525772623@qq.com>
2023-08-27 13:29:39 +0800

    20230827 1329 Jeruisi

--- src/views/GroupPage.vue
+++ src/views/GroupPage.vue
@@ -17,8 +17,8 @@
             <el-container>
               <el-header height="50%" style="display: flex;justify-content: center;align-items: center;">
                 <el-text class="group-title">{{ this.currentGroup.name }}</el-text>
-                <el-button type="primary" @click="this.OpenDialog(1)" style="height: 50px;"><el-icon style="margin-right: 4px;"><Edit /></el-icon>编辑团队信息</el-button>
-                <el-button type="primary" @click="" style="height: 50px;"><el-icon style="margin-right: 4px;"><Edit /><Close /></el-icon>删除团队</el-button>
+                <el-button v-if="this.currentGroup.id!=0" type="primary" @click="this.OpenDialog(1)" style="height: 50px;"><el-icon style="margin-right: 4px;"><Edit /></el-icon>编辑团队信息</el-button>
+                <el-button v-if="this.currentGroup.id!=0" type="primary" @click="this.DeleteGroup(this.currentGroup.id)" style="height: 50px;"><el-icon style="margin-right: 4px;"><Edit /><Close /></el-icon>删除团队</el-button>
               </el-header>
               <el-main>
                 <el-text class="group-introduction">团队简介:{{ this.currentGroup.introduction }}</el-text>
@@ -28,34 +28,36 @@
         </el-container>
       </el-header>
       <el-container class="third-container">
-        <el-aside width="48%" class="bottom-aside">
+        <el-aside width="37%" class="bottom-aside">
           <el-container class="forth-container">
             <el-header style="display: flex;justify-content: center;align-items: center;">
               <el-text class="project-title">项目列表</el-text>
-              <el-button type="primary" @click="this.OpenDialog(2)" style="height: 40px;"><el-icon style="margin-right: 4px;"><Plus /></el-icon>新建项目</el-button>
-              <el-button type="primary" @click="" style="height: 40px;"><el-icon style="margin-right: 4px;"><DeleteFilled /></el-icon>回收站</el-button>
+              <el-button v-if="this.currentGroup.id!=0" type="primary" @click="this.OpenDialog(2)" style="height: 40px;"><el-icon style="margin-right: 4px;"><Plus /></el-icon>新建项目</el-button>
+              <el-button v-if="this.currentGroup.id!=0" type="primary" @click="" style="height: 40px;"><el-icon style="margin-right: 4px;"><DeleteFilled /></el-icon>回收站</el-button>
             </el-header>
             <el-scrollbar>
               <div v-for="item in this.currentGroup.projectCount" :key="item" class="project-list">
                 <el-text class="project-name">{{ this.currentGroup.projectList[item-1].name }}</el-text>
                 <el-text class="creator-name">{{ this.currentGroup.projectList[item-1].creator_name }}创建</el-text>
-                <el-button @click="this.Jump()"><el-icon style="margin-right: 4px;"><Pointer /></el-icon>查看项目</el-button>
+                <el-button @click="this.Jump('/'+this.$store.state.uid+'/MyProject/'+this.currentGroup.projectList[item-1].id)"><el-icon style="margin-right: 4px;"><Pointer /></el-icon>查看项目</el-button>
                 <el-button @click="this.DeleteProject(this.currentGroup.projectList[item-1].id)"><el-icon style="margin-right: 4px;"><Delete /></el-icon>删除项目</el-button>
               </div>
             </el-scrollbar>
           </el-container>
         </el-aside>
-        <el-aside width="48%" class="bottom-aside">
+        <el-aside width="61%" class="bottom-aside">
           <el-container class="forth-container">
             <el-header style="display: flex;justify-content: center;align-items: center;">
               <el-text class="groupmember-title">成员列表</el-text>
-              <el-button type="primary" @click="this.OpenDialog(3)" style="height: 40px;"><el-icon style="margin-right: 4px;"><Plus /></el-icon>邀请新成员</el-button>
+              <el-button v-if="this.currentGroup.id!=0" type="primary" @click="this.OpenDialog(3)" style="height: 40px;"><el-icon style="margin-right: 4px;"><Plus /></el-icon>邀请新成员</el-button>
             </el-header>
             <el-scrollbar>
               <div v-for="item in this.currentGroup.personCount" :key="item"  class="groupmember-list">
-                <el-avatar :size="40" :src="this.currentGroup.personList[item-1].icon_address"/>
+                <el-avatar :size="40" :src="this.currentGroup.personList[item-1].icon_address" fit="fill"/>
                 <el-text class="groupmember-name">{{ this.currentGroup.personList[item-1].username }}</el-text>
-                <el-button @click="this.Jump()"><el-icon style="margin-right: 4px;"><Pointer /></el-icon>查看成员</el-button>
+                <el-text class="groupmember-name">{{ this.currentGroup.personList[item-1].true_name }}</el-text>
+                <el-text class="groupmember-email">{{ this.currentGroup.personList[item-1].email }}</el-text>
+                <el-button @click="this.Jump('/'+this.currentGroup.personList[item-1].id+'/PersonalInfomation')"><el-icon style="margin-right: 4px;"><Pointer /></el-icon>查看成员</el-button>
                 <el-button @click=""><el-icon style="margin-right: 4px;"><Avatar /></el-icon>{{ this.currentGroup.authList[item-1] }}</el-button>
                 <el-button @click=""><el-icon style="margin-right: 4px;"><CircleClose /></el-icon>移除成员</el-button>
               </div>
@@ -209,11 +211,20 @@
   border-bottom: 1px solid #409EFF;
 }
 .groupmember-name{
-  width: 30%;
+  width: 12%;
   margin-left: 20px;
   display: flex;
   text-align: center;
-  font-size:medium;
+  font-size:1ch;
+  font-weight: 500;
+  color: black;
+}
+.groupmember-email{
+  width: 20%;
+  margin-left: 20px;
+  display: flex;
+  text-align: center;
+  font-size:1ch;
   font-weight: 500;
   color: black;
 }
@@ -235,7 +246,7 @@
   border-bottom: 1px solid #409EFF;
 }
 .project-name{
-  width: 25%;
+  width: 20%;
   margin-left: 20px;
   display: flex;
   text-align: center;
@@ -258,6 +269,7 @@
 <script>
 import store from '@/store';
 import { ElMessage } from 'element-plus';
+import { getUserGroup } from '../api/user.js';
 import { createGroup, deleteGroup, changeAuth, inviteMember, getGroupInformation} from '../api/group.js';
 import { createProject, deleteProject } from '../api/project.js';
 
@@ -374,9 +386,30 @@ export default{
     CreateGroup(){
       this.$refs.createGroupRef.validate((valid) => {
         if(valid){
-          var promise=createGroup(this.createGroup.name,this.createGroup.introduction);
-          promise.then((result)=>{
-            this.MessageCatch(result);
+          var promise1=createGroup(this.createGroup.name,this.createGroup.introduction);
+          promise1.then((result)=>{
+            if(this.MessageCatch(result)){
+              var promise2=getUserGroup();
+              promise2.then((result) => {
+                if(result.code==0){
+                  store.commit('updateGroupList',result.data);
+                  this.group.list=result.data;
+                  this.group.length=result.data.length;
+                  if(this.currentGroup.id==0){
+                    this.group.current=0;
+                    this.GetCurrenGroup(this.group.list[0].id);
+                  }
+                  else{
+                    for(var i=0;i<this.group.length;i++){
+                      if(this.group.list[i].id==this.currentGroup.id){
+                        this.group.current=i;
+                        break;
+                      }
+                    }
+                  }
+                }
+              })
+            }
           })
         }
       })
@@ -403,7 +436,6 @@ export default{
     AddNewMember(){
       this.$refs.addNewMemberRef.validate((valid) => {
         if(valid){
-          console.log
           var promise=inviteMember(this.currentGroup.id,this.addNewMember.email);
           promise.then((result) => {
             this.MessageCatch(result);
@@ -411,9 +443,37 @@ export default{
         }
       })
     },
-    EnterProject(projectid){
-      this.$router.push('/'+store.state.uid+'/')
-    }, 
+    DeleteGroup(groupid){
+      var promise1=deleteGroup(groupid);
+      promise1.then((result) => {
+        if(this.MessageCatch(result)){
+          var promise2=getUserGroup();
+          promise2.then((result) => {
+            if(result.code==0){
+              store.commit('updateGroupList',result.data);
+              this.group.list=result.data;
+              this.group.length=result.data.length;
+              console.log(this.group.length);
+              if(this.group.length==0){
+                this.currentGroup.id=0;
+                this.currentGroup.avatar='https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png';
+                this.currentGroup.name='您还没有任何团队';
+                this.currentGroup.introduction='快去创建或加入一个团队吧';
+                this.currentGroup.personCount=0;
+                this.currentGroup.projectCount=0
+                this.currentGroup.personList=[];
+                this.currentGroup.projectList=[];
+                this.currentGroup.authList=[];
+              }
+              else{
+                this.group.current=0;
+                this.GetCurrenGroup(this.group.list[0].id);
+              }
+            }
+          })
+        }
+      })
+    },
     DeleteProject(projectid){
       var promise=deleteProject(projectid);
       promise.then((result) => {
@@ -434,7 +494,6 @@ export default{
         this.currentGroup.projectList=result.data.project_list;
         this.currentGroup.projectCount=this.currentGroup.projectList.length;
         for(var i=0;i<this.currentGroup.personCount;i++){
-          console.log(this.currentGroup.personList[i].position);
           if(this.currentGroup.personList[i].position==='creator'){
             this.currentGroup.authList.push('创建者')
           }
@@ -446,6 +505,27 @@ export default{
           }
         }
       })
+    },
+    ChangeAuth(userid,opcode){
+      var ownPosition;
+      var userPosition;
+      if(userid==store.state.uid){
+        ElMessage({
+          message: "不能对自己进行操作",
+          grouping: true,
+          type: 'error',
+        })
+      }
+      else{
+        for(var i=0;i<this.currentGroup.personCount;i++){
+          if(this.currentGroup.personList[i].id==store.state.uid){
+            ownPosition=this.currentGroup.personList[i].position;
+          }
+          if(this.currentGroup){
+
+          }
+        }
+      }
     },
     SwithcGroup(index){
       this.group.current=index;
@@ -508,11 +588,15 @@ export default{
         break;
       }
       if(i==this.group.length-1){
-
+        if(this.group.length!=0) this.currentGroup.id=this.group.list[0].id;
+        else this.currentGroup.id=0;
       }
     }
     if(this.group.length!=0){
       this.GetCurrenGroup(this.currentGroup.id);
+    }
+    else{
+      this.currentGroup.id=0;
     }
   }
 }
