99e23097c07a2df4016325c5fbfe42420c95fea6
plutoKang <1420246734@qq.com>
2023-08-27 09:06:03 +0800

    20230827 0905 pluto 设计原型基本实现，只差导出jpg

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  devServer: {
+    host: '0.0.0.0',
+  // https:true,
+    port: 6103,
+    client: {
+      webSocketURL: 'ws://0.0.0.0:6103/ws',
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    }
+},
+  transpileDependencies: true,
+  
+  
 })
